a81653bd772c44e5d507f658b77d22f370493bb7
Nomemb <nkj1205@gmail.com>
2021-12-31 13:21:33 +0900

    week 11

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public float turnSpeed = 20f; // 캐릭터가 초당 회전해야 하는 각도
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
+
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity; // 회전을 저장할 수 있음
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+    
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward,
+            m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward); // 해당 방향을 바라보는 회전을 생성
+
+    }
+
+    private void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + 
+            m_Movement * m_Animator.deltaPosition.magnitude);
+        // delatPosition : 루트 모션으로 인한 프레임당 포지션의 이동량
+
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+}
